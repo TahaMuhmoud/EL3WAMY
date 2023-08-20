2375d84090f23a7e427834c8aa5de3cd648c2caa
TahaMuhmoud <elawamytaha@gmail.com>
2023-08-20 19:03:43 +0300

    ss

--- src/Components/Series/TvLandingPage.jsx
+++ src/Components/Series/TvLandingPage.jsx
@@ -136,9 +136,6 @@ const TvLandingPage = ({ seriesData, setLoading }) => {
                 to={homepage ? homepage : `/videonotavailable/tv/${id}`}
                 target="_blank"
                 className="btn cursor-pointer flex gap-1 sm:gap-2 bg-[#ffffff4a] p-3 pr-5 rounded-full hover:bg-[#ffffff00] hover:border-white hover:border-1"
-                onClick={() => {
-                  setLoading(true);
-                }}
               >
                 <VscDebugStart fontSize={30} color="white" />
                 <span className="text-lg sm:text-xl">watch</span>

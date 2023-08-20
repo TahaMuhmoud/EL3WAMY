14d824893a0bed03ad14465f5f371bfe1fbfbbc7
TahaMuhmoud <elawamytaha@gmail.com>
2023-08-20 17:52:32 +0300

    gg

---
+++ src/Components/ToTop/ToTop.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+import { TbCircleArrowUpFilled } from 'react-icons/tb'
+
+function ToTop() {
+  const totopEl = useRef();
+  useEffect(() => {
+    window.onscroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        totopEl.current.classList.remove("hidden");
+      } else {
+        totopEl.current.classList.add("hidden");
+        
+      }
+    }
+  });
+  return (
+    <div
+      className="hidden fixed bottom-24 right-10 rounded-full cursor-pointer"
+      ref={totopEl}
+    >
+      <TbCircleArrowUpFilled
+        fontSize={60}
+        className=" text-mainColor-100"
+        onClick={(e) => {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+        }}
+      />
+    </div>
+  );
+}
+
+export default ToTop
--- src/Pages/RootLayOut/RootLayout.jsx
+++ src/Pages/RootLayOut/RootLayout.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import ToTop from "../../Components/ToTop/ToTop";
 
 export default function RootLayout() {
   return (
-    <div className="app">
+    <div className="app relative">
         <Navbar />
-        <Outlet />
+      <Outlet />
+      <ToTop/>
         <Footer />
     </div>
   );
